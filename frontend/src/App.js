8cfa8ed872c4560956145e85e23a4482df07dded
Funkymartians <mafersaenz2002@gmail.com>
2024-07-28 16:05:38 -0500

    inicio-proyecto

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/')
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the items!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Items</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;

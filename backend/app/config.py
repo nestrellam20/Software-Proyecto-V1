c146d142dacbb4f5b93ce8c35a43b5edd75c119e
nestrellam20 <nathaliebestrellamejia@gmail.com>
2024-07-30 19:55:59 -0500

    Cambiando

--- backend/app/config.py
+++
@@ -1,5 +0,0 @@
-import secrets
-
-class Config:
-    SECRET_KEY = secrets.token_hex(16)  # Generates a random 16-byte hex string
-    MONGO_URI = 'mongodb+srv://msaenzy9:msaenzy9@cluster0.lahyenk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
